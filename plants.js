60079c8b928034a39eadaeb670f5ea49c434ebd0
jayceeannne <eric@example.com>
2023-06-22 14:38:53 -0700

    Initial commit

---
+++ plants.js
@@ -0,0 +1,9 @@
+var gets_sunshine = true
+var ounces_of_water = 10
+
+
+if (gets_sunshine === true && ounces_of_water >= 10){
+    console.log("Plant is thriving!")
+} else {
+    console.log("Plant will die")
+}

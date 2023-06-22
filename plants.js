7f54e4c6980f5bc95c1dfd9085e9c2c216466d7f
jayceeannne <eric@example.com>
2023-06-22 14:42:20 -0700

    Add conditional

--- plants.js
+++ plants.js
@@ -1,9 +1,12 @@
 var gets_sunshine = true
-var ounces_of_water = 10
+var ounces_of_water = 9
 
 
 if (gets_sunshine === true && ounces_of_water >= 10){
     console.log("Plant is thriving!")
+} else if (gets_sunshine === true || ounces_of_water >= 10){
+console .log("Plant is doing ok")
+
 } else {
     console.log("Plant will die")
 }
